f589040bc1360cd237a22db3bc8cf9c0043615a4
shineguo1 <shineguo1@users.noreply.github.com>
2024-07-08 10:29:49 +0800

    feat: EthGetTransactionReceipt

---
+++ common-web3j/src/main/java/org/xj/commons/web3j/req/EthGetTransactionReceiptReq.java
@@ -0,0 +1,35 @@
+package org.xj.commons.web3j.req;
+
+import lombok.Getter;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
+import org.web3j.protocol.core.methods.response.EthTransaction;
+import org.web3j.protocol.core.methods.response.Transaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+
+/**
+ * @author xj
+ * @version 1.0.0 createTime:  2023/2/13 10:37
+ */
+@Getter
+public class EthGetTransactionReceiptReq implements BatchReq, Web3BatchCmd<EthGetTransactionReceipt, TransactionReceipt>{
+
+    private final String transactionHash;
+
+    public EthGetTransactionReceiptReq(String transactionHash){
+        this.transactionHash = transactionHash;
+    }
+
+
+    @Override
+    public Request<?, EthGetTransactionReceipt> getRequest(Web3j web3j, DefaultBlockParameter defaultBlock){
+        return web3j.ethGetTransactionReceipt(transactionHash);
+    }
+
+    @Override
+    public TransactionReceipt getResult(EthGetTransactionReceipt response) {
+        return response.getTransactionReceipt().orElse(null);
+    }
+}
